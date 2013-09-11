7a4b7349239384a56f4466f0a4b5642a10866937
marlonwang <marlonwang@163.com>
2013-09-11 23:35:21 +0800

    new

---
+++ ClientPush.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace ClientTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Client running ...");
+            TcpClient client;
+            try
+            {
+                client = new TcpClient();
+                client.Connect("localhost", 8500);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();//pause
+                return;
+            }
+
+            //打印链接的服务信息
+            Console.WriteLine("Server Connected! {0} --> {1}",
+                client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
+            //string msg = "\"Welcome To TraceFact.Net\"";
+            
+            string msg;
+            //1. 多个客户端一条请求
+            /*
+               msg=Console.ReadLine();
+            */
+
+            //2. 一个客户端多条请求
+            Console.Write("=============================\n");
+            Console.Write("Menu : S - Send, X - Exit  ：\n");
+            Console.Write("=============================\n");
+            Console.Write("[S / X]:");
+            NetworkStream streamToServer = client.GetStream();
+            ConsoleKey key; //判断输入 S或 E
+            try
+            {
+                do
+                {
+                    key = Console.ReadKey(true).Key;
+                    if (key == ConsoleKey.S)
+                    {
+                        Console.Write("\nInput the Message : "); //获取收入字符串
+                        msg = Console.ReadLine();
+
+                        byte[] buffer = Encoding.Unicode.GetBytes(msg); //获取缓存
+                        streamToServer.Write(buffer, 0, buffer.Length);  //发给服务器
+
+                        Console.Write("Sent :{0}", msg);
+                        Console.Write("\nPress s to continue sending message:");
+                    }
+                }
+                while (key != ConsoleKey.X); //X 退出
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();//pause
+                return;
+            }
+            /*
+            byte[] buffer = Encoding.Unicode.GetBytes(msg); //获得缓存
+            streamToServer.Write(buffer, 0, buffer.Length);//发给服务器
+            Console.Write("Sent :{0}", msg);
+            */
+            
+            Console.Read();
+        }
+    }
+}
---
+++ ServerReceive.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int BufferSize = 8192;  //缓存大小  8192字节
+
+            Console.WriteLine("Server  is  running ...");
+            IPAddress ip = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            TcpListener listener = new TcpListener(ip, 8500);
+            //开始侦听
+            listener.Start();
+            Console.WriteLine("Start Listening on port 8500...");
+
+            //1. 多个客户端一条请求
+            /*
+            do
+            {
+                //获取一个连接
+                TcpClient remoteClient = listener.AcceptTcpClient();
+
+                //显示连接
+                Console.WriteLine("Client Connected! {0} <-- {1}",
+                    remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
+
+                //获得流，写入buffer中
+                NetworkStream streamToClient = remoteClient.GetStream();
+                byte[] buffer = new byte[BufferSize];
+                int bytesRead = streamToClient.Read(buffer, 0, BufferSize);
+                Console.WriteLine("Reading data,{0} bytes...", bytesRead);
+
+                //获得请求的字符串
+                string msg = Encoding.Unicode.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("recetved: {0}", msg);
+                
+            }
+            while (true);
+             * 
+            */
+
+            //2. 一个客户端多条请求
+
+            //获取一个连接
+            TcpClient remoteClient = listener.AcceptTcpClient();
+            //显示连接
+            Console.WriteLine("Client Connected! {0} <-- {1}",
+                remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
+
+            //获得流，写入buffer中
+            NetworkStream streamToClient = remoteClient.GetStream();
+            try
+            {
+                do
+                {
+
+                    byte[] buffer = new byte[BufferSize];
+                    int bytesRead = streamToClient.Read(buffer, 0, BufferSize);
+                    Console.WriteLine("Reading data,{0} bytes...", bytesRead);
+
+                    //获取请求的字符串
+                    string msg = Encoding.Unicode.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("recetved: {0}", msg);
+
+                }
+                while (true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();//pause
+                return;
+            }
+
+            //退出
+            Console.WriteLine("输入\"Q\"键退出.");//while 循环 此处执行不到
+
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+            } while (key != ConsoleKey.Q);
+        }
+    }
+}
