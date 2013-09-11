bebb5d923e8f0f3fac1764d04108f4a1d15bd6e5
marlonwang <marlonwang@163.com>
2013-09-11 17:32:45 +0800

    new

---
+++ client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientConsole1
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client Running...");
+            int i;
+            TcpClient client;
+            for (i = 0; i < 5; i++)
+            {
+                try
+                {
+                    client = new TcpClient();
+                    client.Connect("localhost", 8500);
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.Read();
+                    return;
+                }
+
+                //连接信息
+                Console.Write("Server Connected! {0} -->{1}\n",
+                    client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
+            }
+
+            //按q退出
+            Console.WriteLine("\n输入\"Q\"键退出.");
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+            } while (key != ConsoleKey.Q);
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerConsole1
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server is running...");
+            IPAddress ip = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            TcpListener listener = new TcpListener(ip, 8500);
+
+            //监听
+            listener.Start();
+            Console.WriteLine("Start listening on port 8500...");
+
+            //只获取一个客户端 就中断
+            while (true)
+            {
+                TcpClient remoteClient = listener.AcceptTcpClient();
+                Console.WriteLine("Client Connected! {0} <--{1}",
+                    remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
+            }
+
+            Console.WriteLine("输入\"Q\"键退出.");
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+            } while (key != ConsoleKey.Q);
+        }
+    }
+}
